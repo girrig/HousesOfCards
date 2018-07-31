749dbdcc3a59cbfceb8f7304f7fbcd544664d80b
Brandon Odom <girrig3@gmail.com>
2018-07-31 16:08:36 -0500

    Classes + Setup Phase
    Globals Created
    Added Player and Card classes
    Game Init and Setup Phases competed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import random
 from termcolor import colored, cprint
+from operator import itemgetter
 
 # DEV GLOBALS
 debug = False  # Autoplay
@@ -9,9 +10,10 @@ debug = False  # Autoplay
 # META GLOBALS
 num_players = None
 players = []  # Array of Player objects
+player_going_first = None
 
 # GAME GLOBALS
-cur_player = None  # No Player 0
+cur_player = None  # Starts at 0
 phase = None  # 0=Setup; 1=Plotting; 2=Questing; 3=Cleanup
 deck = []  # Array of Card objects
 calamity = None  # 1-100
@@ -33,17 +35,36 @@ class Player:
     def __init__(self, name, p_num, affinities, points, hand):
         self.name = name
         self.p_num = p_num
-        self.affinities = affinities
+        self.affinities = affinities  #Array of ints; Ordering is: Spade, Heart, Club, Diamond
         self.points = points
         self.hand = hand
+
+    def __str__(self):
+        return  str(self.__class__) + '\n'+ '\n'.join(('{} = {}'.format(item, self.__dict__[item]) for item in self.__dict__))
+
+    def __repr__(self):
+        return self.__str__()
 
     def getName(self):
         print 'Player ' + str(self.p_num) + ' is ' + self.name + '.'
 
+    def getHighestValueCard(self):
+        current_highest = 0
+        for card in self.hand:
+            if card.value > current_highest:
+                current_highest = card.value
+        return current_highest
+
 class Card:
     def __init__(self, value, suit):
-        self.value = value  # 1-13; where 1 is joker, 2-10 are 2-10, and 11-14 are Jack, Queen, King, Ace respectively
+        self.value = value  # 1-13; where 1 is Joker, 2-10 are 2-10, and 11-14 are Jack, Queen, King, Ace respectively
         self.suit = suit
+
+    def __str__(self):
+        return  str(self.__class__) + '\n'+ '\n'.join(('{} = {}'.format(item, self.__dict__[item]) for item in self.__dict__))
+
+    def __repr__(self):
+        return self.__str__()
 
     def getName(self):
         if 1 < self.value < 11:
@@ -59,17 +80,17 @@ class Card:
         elif self.value == 1:
             print 'The card is the ' + self.suit + ' Joker.'
         else:
-            print 'ERROR: something is fucked in the getName() function'
+            print 'ERROR: Something is fucked in the Card Class getName() function'
 
 def printPlayers():
     for player in players:
         player.getName()
     return
 
-def createDeck():
-    for suit in ('Diamond','Heart','Spade','Club'):
-        for num in xrange(2,14):
-            card = Card(num, suit)
+def dealDeck():
+    for suit in ('Spade','Heart','Club','Diamond'):
+        for value in xrange(2,14):
+            card = Card(value, suit)
             deck.append(card)
     joker1 = Card(1, 'Non-Colored')
     joker2 = Card(1, 'Colored')
@@ -77,7 +98,7 @@ def createDeck():
     deck.append(joker2)
     return
 
-def shuffleCards():
+def shuffleDeck():
     random.shuffle(deck)
     return
 
@@ -87,8 +108,8 @@ def printDeck():
     return
 
 def initGame():
-    createDeck()
-    shuffleCards()
+    dealDeck()
+    shuffleDeck()
     return
 
 
@@ -102,17 +123,17 @@ input = raw_input('Enter number of players: ')
 
 if input == '0':
     debug = True
+    num_players = 0
 else:
     num_players = int(input)
-    for num in xrange(1,num_players+1):
-        name = raw_input('Enter name for Player ' + str(num) + ':' )
-        player = Player(name, num, [], 0, [])
+    for num in xrange(0,num_players):
+        name = raw_input('Enter name for Player ' + str(num+1) + ':' )
+        player = Player(name, num, [0,0,0,0], 0, [])
         players.append(player)
 
 
 
 # GAME START
-phase = 0
 
 # Game Loop
 if debug:
@@ -122,6 +143,7 @@ if debug:
 
 if not debug:
     while True:
+        """
         print_grey('yo this is a ' + str(10) + ' sentence')
         print_red('yo this is a ' + str(10) + ' sentence')
         print_green('yo this is a ' + str(10) + ' sentence')
@@ -130,22 +152,105 @@ if not debug:
         print_magenta('yo this is a ' + str(10) + ' sentence')
         print_cyan('yo this is a ' + str(10) + ' sentence')
         print_white('yo this is a ' + str(10) + ' sentence')
+        """
 
         #Set current player
-        #cur_player = 1
+        if cur_player == None:
+            cur_player = 0
+        elif cur_player == num_players:
+            cur_player = 0
+        else:
+            cur_player = cur_player + 1
+
+        #Set current phase
+        if phase == None:
+            phase = 0
+        elif phase == 3:
+            phase = 1
+        else:
+            phase = phase + 1
 
         # SETUP PHASE
         if phase == 0:
             #Each player draws 3 cards
+            for player in players:
+                for i in xrange(3):
+                    player.hand.append(deck.pop())
+
             #Each player games +1 affinity to the drawn card suits
+            for player in players:
+                for card in player.hand:
+                    if card.suit == 'Spade':
+                        player.affinities[0] = player.affinities[0] + 1
+                    elif card.suit == 'Heart':
+                        player.affinities[1] = player.affinities[1] + 1
+                    elif card.suit == 'Club':
+                        player.affinities[2] = player.affinities[2] + 1
+                    elif card.suit == 'Diamond':
+                        player.affinities[3] = player.affinities[3] + 1
+                    else:
+                        print_red('ERROR: Affinity attribution is broken!')
+
             #The player with the highest value card drawn goes first
-                #Keep drawing to break ties
+            high_num_buf = []
+            for player in players:
+                temp_tuple = (player.p_num, player.getHighestValueCard())
+                high_num_buf.append(temp_tuple)
+            for num in xrange(len(high_num_buf)):
+                max(high_num_buf,key=itemgetter(1))
+            is_tie = False
+            tied_player_buf = []
+            for i,j in enumerate(high_num_buf):
+                if j[1] == max(high_num_buf,key=itemgetter(1))[1]:
+                    tied_player_buf.append(j)
+            if len(tied_player_buf) > 1:
+                is_tie = True
+
+            #Keep drawing to break ties
+            if is_tie:
+                while player_going_first == None:
+                    for player in players:
+                        player.hand = []
+
+                    for i in xrange(0,len(tied_player_buf)-1):
+                        players[tied_player_buf[i][0]].hand.append(deck.pop())
+
+                    high_num_buf = []
+                    for player in players:
+                        temp_tuple = (player.p_num, player.getHighestValueCard())
+                        high_num_buf.append(temp_tuple)
+                    for num in xrange(len(high_num_buf)):
+                        max(high_num_buf,key=itemgetter(1))
+                    tied_player_buf = []
+                    for i,j in enumerate(high_num_buf):
+                        if j[1] == max(high_num_buf,key=itemgetter(1))[1]:
+                            tied_player_buf.append(j)
+
+                    if len(tied_player_buf) > 1:
+                        pass
+                    else:
+                        player_going_first = tied_player_buf[0][0]
+
             #Empty player hands
+            for player in players:
+                player.hand = []
+
             #Shuffle the deck
+            deck = []
+            dealDeck()
+            shuffleDeck()
+
             #Each player draws 5 cards
+            for player in players:
+                for i in xrange(5):
+                    player.hand.append(deck.pop())
 
         # PLOTTING PHASE
-        if phase == 1:
+        elif phase == 1:
+            print_green('Got to phase 1')
+            print_white(players[0])
+            print_white(players[1])
+            pass
             #Choose 1
                 #Current player draws a resource cards
                 #Current player offers to trade to the table
@@ -153,7 +258,8 @@ if not debug:
                 #Current player buys a Usurper's Chance card
 
         # QUESTING PHASE
-        if phase == 2:
+        elif phase == 2:
+            pass
             #Current player chooses 1
                 #Current player attempts to stop calamity by spending a resource card
                     #Current player can only spend 1 resource card
@@ -189,7 +295,11 @@ if not debug:
                             #Current player wins the game
 
         # CLEANUP PHASE
-        if phase == 3:
+        elif phase == 3:
+            tempy = input('block')
             #If the calamity was stopped, roll a d100 for new calamity; otherwise pass
             #If the deck is empty and all players hands are empty, the player with the most victory points wins
                 #Usurper Chance cards count for 10 victory points
+
+        else:
+            print_red('ERROR: Phase changing is broken!')
---
+++ test.py
@@ -0,0 +1,26 @@
+from operator import itemgetter
+
+
+
+'''
+#[i for i, j in enumerate(a) if j == max(a)]
+
+list = [1,2,7,4,5,2,6,2,2,1,7,3] #2,10
+
+result = []
+for i,j in enumerate(list):
+    if j == max(list):
+        result.append(i)
+print result
+'''
+
+
+list = [(0,5),(1,10),(2,10)] #(1,10),(2,10)
+
+result = []
+for i,j in enumerate(list):
+    #print j[1]
+    #print max(list,key=itemgetter(1))
+    if j[1] == max(list,key=itemgetter(1))[1]:
+        result.append(j)
+print result
