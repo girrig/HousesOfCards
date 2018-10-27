53db43291813fd1774c18c518384e9259aceacd4
Brandon Odom <girrig3@gmail.com>
2018-10-27 19:13:52 -0400

    flake8 Refactor
    -Refactored the code to comply with the python3.7 flake8 linter
    -Fixed bug where all players where included in the tie breaker
    -Fixed a range looping bug becuase I'm and idiot and thought you had to -1 the list length
    -Various bug and naming fixes

--- test.py
+++ test.py
@@ -1,26 +1,23 @@
 from operator import itemgetter
 
 
+# REF: [i for i, j in enumerate(a) if j == max(a)]
 
-'''
-#[i for i, j in enumerate(a) if j == max(a)]
-
-list = [1,2,7,4,5,2,6,2,2,1,7,3] #2,10
-
-result = []
-for i,j in enumerate(list):
-    if j == max(list):
-        result.append(i)
-print result
-'''
+# list = [1, 2, 7, 4, 5, 2, 6, 2, 2, 1, 7, 3]  # 2,10
+#
+# result = []
+# for i, j in enumerate(list):
+#     if j == max(list):
+#         result.append(i)
+# print(result)
 
 
-list = [(0,5),(1,10),(2,10)] #(1,10),(2,10)
+list = [(0, 5), (1, 10), (2, 10)]  # (1,10),(2,10)
 
 result = []
-for i,j in enumerate(list):
-    #print j[1]
-    print max(list,key=itemgetter(1))
-    if j[1] == max(list,key=itemgetter(1))[1]:
+for i, j in enumerate(list):
+    # print j[1]
+    print(max(list, key=itemgetter(1)))
+    if j[1] == max(list, key=itemgetter(1))[1]:
         result.append(j)
-print result
+print(result)
